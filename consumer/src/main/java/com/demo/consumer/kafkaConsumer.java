565870e6bdddf241264d5fe46aec25e8d2ede13d
SachinBhagat1617 <contactbhagatsachin@gmail.com>
2025-10-05 12:36:22 +0530

    Added Kafka Topic creation config

--- consumer/src/main/java/com/demo/consumer/kafkaConsumer.java
+++ consumer/src/main/java/com/demo/consumer/kafkaConsumer.java
@@ -6,8 +6,20 @@ import org.springframework.stereotype.Component;
 @Component
 public class kafkaConsumer {
     @KafkaListener(topics = "my-topic", groupId = "my-new-group")
-    public void listen(String message){
-        System.out.println("Received Message: " +message);
+    public void listen1(String message){
+        System.out.println("Received Message from listen1: " +message);
+    }
+    // this helps in loadBalancing since there are multiple consumer in same groupId
+    // so any one of them will receive the message but if there are different groupId or group
+    // listening to same topic then both the group will receive message like broadcasting.
+    @KafkaListener(topics = "my-topic", groupId = "my-new-group")
+    public void listen2(String message){
+        System.out.println("Received Message from listen2: " +message);
+    }
+
+    @KafkaListener(topics = "my-topic", groupId = "my-new-group2")
+    public void listen3(String message){
+        System.out.println("Received Message from listen3: " +message);
     }
 
 }
